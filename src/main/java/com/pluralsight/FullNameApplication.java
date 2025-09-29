1095366791762635dbc508fb01270a2c7d38a531
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-29 10:48:56 -0400

    * initialized variables * added sout statements asking user for first and last name * added functions to add user input to variables * made a souf statement to print out user first and last name

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class FullNameApplication {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        String firstName;
+        String lastName;
+        String middleInitial;
+        String nameSuffix;
+
+        System.out.print("Please enter your first name: ");
+        firstName = keyboard.nextLine();
+        firstName = firstName.trim();
+        System.out.print("Pleas enter your last name: ");
+        lastName = keyboard.nextLine();
+        lastName = lastName.trim();
+
+        System.out.printf("\nYour name is: %s %s", firstName, lastName);
+
 
     }
 }
